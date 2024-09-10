ea903bbc866647a1f770cc524164b147e1295f95
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-09-10 19:41:43 +0600

    first added

---
+++ src/LibraryManagement/DB/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.DB
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options): base(options) 
+        { 
+
+        }
+        public DbSet<Library> MyProperty {  get; set; }
+    }
+}
